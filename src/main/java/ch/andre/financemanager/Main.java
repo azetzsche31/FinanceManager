17c92f31a74951bd3625a429067f65937f264aa9
Andreas Zetzsche <andreaszetzsche31@gmail.com>
2026-07-15 23:42:06 +0200

    Projektgrundgerüst mit Maven eingerichtet

---
+++ src/main/java/ch/andre/financemanager/Main.java
@@ -0,0 +1,9 @@
+package ch.andre.financemanager;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Finanace Manager startet erfolgreich.");
+    }
+}
