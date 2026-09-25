ff1803098613da1ed350df78992d9451e68094f0
Andreas Zetzsche <andreaszetzsche31@gmail.com>
2026-09-25 22:18:45 +0200

    Connect JavaFX dashboard to persisted account

--- src/main/java/ch/andre/financemanager/FinanceManagerApp.java
+++ src/main/java/ch/andre/financemanager/FinanceManagerApp.java
@@ -1,5 +1,6 @@
 package ch.andre.financemanager;
 
+import ch.andre.financemanager.model.AccountType;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -9,10 +10,44 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import ch.andre.financemanager.model.Account;
+import ch.andre.financemanager.persistence.AccountLoader;
+import ch.andre.financemanager.persistence.AccountRepository;
+import ch.andre.financemanager.persistence.DatabaseManager;
+import ch.andre.financemanager.persistence.TransactionRepository;
+
+import java.sql.SQLException;
+
+import ch.andre.financemanager.service.FinanceService;
+
+import java.math.BigDecimal;
+
 public class FinanceManagerApp extends Application{
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws  SQLException {
+        FinanceService financeService = new FinanceService();
+
+        DatabaseManager databaseManager =
+                new DatabaseManager("finanace-manager.db");
+
+        databaseManager.createAccountsTable();
+        databaseManager.createTransactionsTable();
+
+        AccountRepository accountRepository =
+                new AccountRepository(databaseManager);
+
+        TransactionRepository transactionRepository =
+                new TransactionRepository(databaseManager);
+
+        AccountLoader accountLoader =
+                new AccountLoader(
+                        accountRepository,
+                        transactionRepository
+                );
+
+        Account account = accountLoader.loadOrCreateDefaultAccount();
+
         BorderPane root = new BorderPane();
 
         Label titleLabel = new Label("Finanace Manager");
@@ -24,7 +59,12 @@ public class FinanceManagerApp extends Application{
         root.setTop(header);
 
         Label balanceTitle = new Label("Kontostand");
-        Label balanceValue = new Label("0.00 CHF");
+
+        BigDecimal balance = financeService.calculateBalance(account);
+
+        Label balanceValue = new Label(
+                balance + " " + account.getCurrency().getCurrencyCode()
+        );
 
         VBox balanceBox = new VBox(10);
         balanceBox.getChildren().addAll(
